c8469e513fb4d5635f2f7e46babbcbc3a0cc82e5
Suresh Bugaliya <152490294+SureshBugaliya877@users.noreply.github.com>
2024-05-10 04:15:44 +0000

    learn variables in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 8565
+let accountEmail = "sureshbugaliya@gmail.com"
+var accountNumber = "9610238230" // dont use this variables becouse of issue on blokes and functions .....
+accountCity = "jaipur"
+let accountState;   // undefind  
+
+// accountID = 6 // dont change const value 
+//console.log(accountID);
+
+
+accountEmail = "suresh@gmail.com"
+accountNumber = "9024765005"
+accountCity = "pune"
+
+console.table([accountEmail, accountID, accountNumber, accountCity, accountState]);
+
+
+
+
+
+
